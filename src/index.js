abc221e8a6238098d81471d5a649e559b0b9064e
zeruel11 <the.zero0110@gmail.com>
2018-09-21 15:45:46 +0700

    initial

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import ToDo from "./ToDo";
+// import registerServiceWorker from "./registerServiceWorker";
+
+ReactDOM.render(<ToDo />, document.getElementById("root"));
+// registerServiceWorker();
